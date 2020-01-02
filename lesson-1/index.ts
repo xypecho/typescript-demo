4008f35b00676a30192f41088da4ad1d7f7ed54f
xypecho <xypecho@163.com>
2020-01-02 21:34:32 +0800

    初始化

---
+++ lesson-1/index.ts
@@ -0,0 +1,3 @@
+// tsc --init 可以生成tsconfig.json 监视tsc文件，实时转成js文件
+
+var str: string = 'hello world'
---
+++ lesson-1/js/index.js
@@ -0,0 +1,3 @@
+"use strict";
+// tsc --init 可以生成tsconfig.json 监视tsc文件，实时转成js文件
+var str = 'hello world';
